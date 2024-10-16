19368ce3827d4885a2ae1e8fa066787e87628e23
Alex Abraham <alexab6104@gmail.com>
2024-10-16 15:00:33 -0400

    Basic Functionality + Design

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
-import express from "express"
-import bodyParser from "body-parser"
+import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+let blogs = []; // Use an array to store multiple blog entries
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.use(express.static("public")); 
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index.ejs"); 
-});
-  
-
-app.post("/create", (req, res) => {
-    res.render("CreateBlog.ejs")
+    res.render("index");
 });
 
-app.post("/view", (req, res) => {
-    res.render("viewBlog.ejs")
+app.get("/create", (req, res) => {
+    res.render("createBlog");
+});
+
+app.get("/view", (req, res) => {
+    res.render("viewBlog", { count: blogs.length, blogs: blogs });
+});
+
+app.post("/submit", (req, res) => {
+    const newBlog = {
+        authorName: req.body.name,
+        authorEmail: req.body.email,
+        blogTitle: req.body["blog-title"],
+        blogText: req.body["blog-text"]
+    };
+
+    blogs.push(newBlog);
+
+    res.redirect("/view");
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
 
-app.post("/submit", (req, res) => {  
-    
-    res.render("viewBlog.ejs", 
-      {
-        authorName : req.body["name"], 
-        authorEmail : req.body["email"], 
-        blogTitle : req.body["blog-title"],
-        blogText : req.body["blog-text"], 
-        
-      });       
-  });
